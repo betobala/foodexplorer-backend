5051faf01c6af0c5f8b03f81f4356148f11bc361
Humberto Abreu <beto199902@gmail.com>
2023-07-19 18:00:53 -0300

    pm2 added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 const AppError = require("./utils/AppError")
 const uploadConfig = require("./configs/upload")
 
@@ -30,6 +31,6 @@ app.use(( error, request, response, next) => {
   })
 })
 
-const PORT = 2222;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
 
